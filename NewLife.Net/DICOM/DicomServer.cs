22366709a86c39305834a45745bdafd6ab7438e2
大石头 <nnhy@vip.qq.com>
2019-02-11 10:44:18 +0800

    新增DICOM协议框架

---
+++ NewLife.Net/DICOM/DicomServer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewLife.Net.DICOM
+{
+    /// <summary>DICOM服务端。管理所有网络会话</summary>
+    public class DicomServer: NetServer<DicomSession>
+    {
+        /// <summary>实例化服务端</summary>
+        public DicomServer()
+        {
+            // 默认端口
+            Port = 104;
+        }
+    }
+}
---
+++ NewLife.Net/DICOM/DicomSession.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewLife.Net.DICOM
+{
+    /// <summary>DICOM会话。每一个远程连接唯一对应一个网络会话，再次重复收发信息</summary>
+    public class DicomSession : NetSession<DicomServer>
+    {
+        ///// <summary>客户端连接</summary>
+        //public override void Start()
+        //{
+        //    base.Start();
+
+        //    // 欢迎语
+        //    var str = String.Format("Welcome to visit {1}!  [{0}]\r\n", Remote, Environment.MachineName);
+        //    Send(str);
+        //}
+
+        /// <summary>收到客户端数据</summary>
+        /// <param name="e"></param>
+        protected override void OnReceive(ReceivedEventArgs e)
+        {
+            //WriteLog("收到：{0}", e.Packet.ToStr());
+
+            // 把收到的数据发回去
+            Send(e.Packet);
+        }
+    }
+}
