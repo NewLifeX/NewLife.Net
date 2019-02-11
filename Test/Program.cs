12ecdeff847360f90d475197d4e2dbeda0b4dd06
大石头 <nnhy@vip.qq.com>
2019-02-11 10:56:10 +0800

    增加dicom例程

---
+++ Test/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NewLife.Log;
+using NewLife.Net.DICOM;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XTrace.UseConsole();
+
+            try
+            {
+                Test1();
+            }
+            catch (Exception ex)
+            {
+                XTrace.WriteException(ex);
+            }
+
+            Console.WriteLine("OK!");
+            Console.ReadKey();
+        }
+
+        static void Test1()
+        {
+            var server = new DicomServer();
+            server.Log = XTrace.Log;
+            server.SessionLog = XTrace.Log;
+            server.SocketLog = XTrace.Log;
+            server.Start();
+        }
+    }
+}
