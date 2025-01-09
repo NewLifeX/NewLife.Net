197a6e355d477da69d2b261b125ce350a2fa7a07
智能大石头 <nnhy@vip.qq.com>
2025-01-09 23:43:52 +0800

    修改全局Utility扩展的命名空间为NewLife

--- RpcTest/UserController.cs
+++ RpcTest/UserController.cs
@@ -1,71 +1,71 @@
 ﻿using System;
+using NewLife;
 using NewLife.Log;
 using NewLife.Remoting;
 using NewLife.Security;
 
-namespace RpcTest
+namespace RpcTest;
+
+/// <summary>用户控制器。会话获取，请求过滤</summary>
+[Api("User")]
+class UserController : IApi, IActionFilter
 {
-    /// <summary>用户控制器。会话获取，请求过滤</summary>
-    [Api("User")]
-    class UserController : IApi, IActionFilter
+    /// <summary>会话。同一Tcp/Udp会话多次请求共用，执行服务方法前赋值</summary>
+    public IApiSession Session { get; set; }
+
+    [Api(nameof(FindByID))]
+    public User FindByID(Int32 uid, Boolean enable)
     {
-        /// <summary>会话。同一Tcp/Udp会话多次请求共用，执行服务方法前赋值</summary>
-        public IApiSession Session { get; set; }
+        // Session 用法同Web
+        var times = Session["Times"].ToInt();
+        times++;
+        Session["Times"] = times;
 
-        [Api(nameof(FindByID))]
-        public User FindByID(Int32 uid, Boolean enable)
+        // 故意制造异常
+        if (times >= 2)
         {
-            // Session 用法同Web
-            var times = Session["Times"].ToInt();
-            times++;
-            Session["Times"] = times;
+            // 取得当前上下文
+            var ctx = ControllerContext.Current;
 
-            // 故意制造异常
-            if (times >= 2)
-            {
-                // 取得当前上下文
-                var ctx = ControllerContext.Current;
-
-                throw new ApiException(507, $"[{ctx.ActionName}]调用次数过多！Times={times}");
-            }
-
-            var user = new User
-            {
-                ID = uid,
-                Name = Rand.NextString(8),
-                Enable = enable,
-                CreateTime = DateTime.Now,
-            };
-
-            return user;
+            throw new ApiException(507, $"[{ctx.ActionName}]调用次数过多！Times={times}");
         }
 
-        /// <summary>本控制器执行前</summary>
-        /// <param name="filterContext"></param>
-        public void OnActionExecuting(ControllerContext filterContext)
+        var user = new User
         {
-            // 请求参数
-            var ps = filterContext.Parameters;
+            ID = uid,
+            Name = Rand.NextString(8),
+            Enable = enable,
+            CreateTime = DateTime.Now,
+        };
 
-            // 服务参数
-            var cs = filterContext.ActionParameters;
+        return user;
+    }
 
-            foreach (var item in ps)
-            {
-                if (cs != null && !cs.ContainsKey(item.Key))
-                    XTrace.WriteLine("服务[{0}]未能找到匹配参数 {1}={2}", filterContext.ActionName, item.Key, item.Value);
-            }
+    /// <summary>本控制器执行前</summary>
+    /// <param name="filterContext"></param>
+    public void OnActionExecuting(ControllerContext filterContext)
+    {
+        // 请求参数
+        var ps = filterContext.Parameters;
+
+        // 服务参数
+        var cs = filterContext.ActionParameters;
+
+        foreach (var item in ps)
+        {
+            if (cs != null && !cs.ContainsKey(item.Key))
+                XTrace.WriteLine("服务[{0}]未能找到匹配参数 {1}={2}", filterContext.ActionName, item.Key, item.Value);
         }
+    }
 
-        /// <summary>本控制器执行后，包括异常发生</summary>
-        /// <param name="filterContext"></param>
-        public void OnActionExecuted(ControllerContext filterContext)
+    /// <summary>本控制器执行后，包括异常发生</summary>
+    /// <param name="filterContext"></param>
+    public void OnActionExecuted(ControllerContext filterContext)
+    {
+        var ex = filterContext.Exception;
+        if (ex != null && !filterContext.ExceptionHandled)
         {
-            var ex = filterContext.Exception;
-            if (ex != null && !filterContext.ExceptionHandled)
-            {
-                XTrace.WriteLine("控制器拦截到异常：{0}", ex.Message);
-            }
+            XTrace.WriteLine("控制器拦截到异常：{0}", ex.Message);
         }
     }
 }
